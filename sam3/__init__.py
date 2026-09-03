4546541c64826d092f82fd31430ea30ab6d81e4f
hagikehappy <hagikehappy@163.com>
2026-09-03 10:10:19 +0800

    update to v0.1.0+hvstudio.7

--- sam3/__init__.py
+++ sam3/__init__.py
@@ -4,6 +4,6 @@
 
 from .model_builder import build_sam3_image_model, build_sam3_predictor
 
-__version__ = "0.1.0+hvstudio.6"
+__version__ = "0.1.0+hvstudio.7"
 
 __all__ = ["build_sam3_image_model", "build_sam3_predictor"]
